0294ff9cb194c9b56bf1f773944126bc134bf8c1
PogorelskiyGrigoriy <pogorelskiygrigoriy@gmail.com>
2026-04-26 12:49:33 +0300

    chore: sortedSet example

--- src/sorted_list_example.py
+++ src/sorted_list_example.py
@@ -1,6 +1,8 @@
 from sortedcontainers import SortedList
 
-my_sorted_list = SortedList([5, 3, 1, 4, 2])
+my_sorted_list = SortedList([5,5,5,5, 3, 1, 4, 2])
 print("Sorted List:", my_sorted_list)
 my_sorted_list.add(0)
 print("After adding 0:", my_sorted_list)
+
+print(list(reversed(my_sorted_list)))
---
+++ src/sorted_set_example.py
@@ -0,0 +1,4 @@
+from sortedcontainers import SortedSet
+
+my_sorted_set = SortedSet(["banana", "apple", "cherry", "Apple"])
+print("Sorted Set:", my_sorted_set)
