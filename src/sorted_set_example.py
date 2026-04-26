008815903f02039792a43571a53774c41ed8d0c5
PogorelskiyGrigoriy <pogorelskiygrigoriy@gmail.com>
2026-04-26 13:46:15 +0300

    chore: sortedDIct example

--- src/sorted_set_example.py
+++ src/sorted_set_example.py
@@ -1,4 +1,4 @@
 from sortedcontainers import SortedSet
 
-my_sorted_set = SortedSet(["banana", "apple", "cherry", "Apple"])
+my_sorted_set = SortedSet(["banana", "apple", "cherry", "Apple", "Apricot"])
 print("Sorted Set:", my_sorted_set)
