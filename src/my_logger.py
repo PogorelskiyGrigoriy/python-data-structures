863128466d5103e98dec79ac252ff30476ca6ee1
PogorelskiyGrigoriy <pogorelskiygrigoriy@gmail.com>
2026-04-28 12:00:31 +0300

    init: education logger branch

---
+++ src/my_logger.py
@@ -0,0 +1,6 @@
+from loguru import logger
+
+logger.info("Loguru установлена через uv!")
+logger.debug("Это сообщение для отладки")
+logger.error("А так выглядит ошибка")
+logger.warning("Это предупреждение")
