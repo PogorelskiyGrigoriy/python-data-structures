db5969e6b225ab7cb183fd3679351b7716dac101
PogorelskiyGrigoriy <pogorelskiygrigoriy@gmail.com>
2026-04-29 11:30:44 +0300

    feat: prototype pattern v2

--- src/prototype.py
+++ src/prototype.py
@@ -1,14 +1,29 @@
-proto_user = {
-    "role": "user",
-    "features": ["read"]}
-proto_admin = {
-    "role": "admin",
-    "features": ["read", "write", "delete"]}
+import copy
+from logger_config import setup_logging
 
-def create_user(role: str, name: str) -> dict:
-    if role == "user":
-        return proto_user.copy()
-    elif role == "admin":
-        return proto_admin.copy()
-    else:
-        raise ValueError("Unknown role")
+logger = setup_logging()
+
+class Monster:
+    def __init__(self, name: str, skills: list[str]):
+        self.name = name
+        self.skills = skills
+
+    def __str__(self):
+        return f"Monster {self.name} with skills: {self.skills}"
+
+    def clone(self, **kwargs):
+        # Используем deepcopy, чтобы вложенные списки тоже скопировались
+        obj = copy.deepcopy(self)
+        # Позволяем изменить некоторые поля при клонировании
+        obj.__dict__.update(kwargs)
+        return obj
+
+# 1. Создаем "Прототип" (эталон)
+basic_orc = Monster("Orc Warrior", ["Strong", "Green"])
+
+# 2. Клонируем его
+elite_orc = basic_orc.clone(name="Elite Orc")
+elite_orc.skills.append("Berserk")
+
+logger.info(f"Original: {basic_orc}")
+logger.info(f"Clone: {elite_orc}")
